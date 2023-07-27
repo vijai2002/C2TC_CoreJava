7476ac6519dd5a94011c75f434d911e09920fdff
Vijai <Vijai@MUSICLOVER>
2023-07-27 14:37:29 +0530

    Constructor

--- Developer/src/com/cg/dyone/Basics.java
+++ Developer/src/com/cg/dyone/Basics.java
@@ -3,19 +3,29 @@ package com.cg.dyone;
 import java.util.Scanner;
 
 public class Basics {
+	Basics()
+	{
+		System.out.println("HI");
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-
-		Scanner sc=new Scanner(System.in);
-		int a,b;
-		System.out.println("Enter the value of A:");
-		a=sc.nextInt();
-		System.out.println("Enter the value of b:");
-		b=sc.nextInt();
-		int c=a+b;
-		System.out.println(c);
-		
+//		System.out.println("Hello World");
+//
+//		Scanner sc=new Scanner(System.in);
+//		int a,b;
+//		System.out.println("Enter the value of A:");
+//		a=sc.nextInt();
+//		System.out.println("Enter the value of b:");
+//		b=sc.nextInt();
+//		int c=a+b;
+//		System.out.println(c);
+		Access ad=new Access();
+		System.out.println(ad.i);
+		System.out.println(ad.j);
+		//System.out.println(ad.k);
+		System.out.println(ad.l);
+		Basics s=new Basics();
+		//Basics();
 		
 		
 	}
---
+++ Developer/src/com/cg/dyone/Constructor.java
@@ -0,0 +1,23 @@
+package com.cg.dyone;
+
+import java.security.PublicKey;
+
+public class Constructor {
+
+	//Constructor
+	Constructor()
+	{
+		System.out.println("Hello! This is constructor");
+	}
+	//Normal method
+	void C()
+	{
+		System.out.println("This is normal method");
+	}
+	public static void main(String[] args)
+	{
+		Constructor cs=new Constructor ();
+		cs.C();
+		
+	}
+}
